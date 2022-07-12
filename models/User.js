56fe48a4f00451029b225be74f4cd571330e4b06
Ashley Smith <ashlynn4567@gmail.com>
2022-07-12 13:18:03 -0500

    setup index file within models to create associations. also created user model

---
+++ models/User.js
@@ -0,0 +1,79 @@
+// imports
+const bcrypt = require("bcrypt");
+const sequelize = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
+
+// create User model
+class User extends Model{
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPW) {
+        return bcrypt.compareSync(loginPW, this.password);
+    };
+};
+
+// define table columns and configuration
+User.init(
+    // table column definitions
+    {
+        id: {
+            type: DataTypes.INTEGER, 
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        }, 
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: {
+                args: true, 
+                msg: "This username is already taken!",
+            },
+        }, 
+        email: {
+            type: DataTypes.STRING,
+            validate: {
+                isEmail: {
+                    args: true,
+                    msg: "You must enter a valid email address!",
+                },
+            },
+            unique: {
+                args: true, 
+                msg: "This username is already taken!",
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [6],
+                    msg: "Your password must contain at least 6 characters!"
+                },
+            },
+        },
+    }, 
+    // table config options 
+    {
+        hooks: {
+            // functionality to hash password before it is saved into database
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // hash password after it is updated and before it is re-saved into database
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            },
+        }, 
+        sequelize, 
+        timestamps: false, 
+        freezeTableName: true,
+        underscored: true, 
+        modelName: "user"
+    }
+);
+
+// exports
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,16 @@
+// import models
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+// define model associations
+// User
+
+
+// Post
+
+
+// Comment
+
+
+module.exports = { User, Post, Comment };
