509aff705a48ab483c17be4088f54881a66daaf2
Ashley Smith <ashlynn4567@gmail.com>
2022-07-12 13:42:30 -0500

    created model associations between user, post, and comment

--- models/index.js
+++ models/index.js
@@ -5,12 +5,33 @@ const Comment = require("./Comment");
 
 // define model associations
 // User
+User.hasMany(Post, {
+    foreignKey: "userId",
+});
 
+User.hasMany(Comment, {
+    foreignKey: "userId",
+});
 
 // Post
+Post.belongsTo(User, {
+    foreignKey: "userId", 
+    onDelete: "CASCADE",
+});
 
+Post.hasMany(Comment, {
+    foreignKey: "postId",
+});
 
 // Comment
+Comment.belongsTo(User, {
+    foreignKey: "userId",
+    onDelete: "CASCADE",
+});
 
+Comment.belongsTo(Post, {
+    foreignKey: "postId", 
+    onDelete: "CASCADE",
+});
 
 module.exports = { User, Post, Comment };
