4eb091407aefe52c091140e75cc409067e307c83
Ashley Smith <ashlynn4567@gmail.com>
2022-07-13 11:35:34 -0500

    added login, signup, and logout front-end javascript functionality

---
+++ public/javascript/logout.js
@@ -0,0 +1,16 @@
+// logout button handler
+async function logout() {
+    const response = await fetch("/api/users/logout", {
+        method: "POST", 
+        headers: { "Content-Type": "application/json" }
+    });
+
+    if (response.ok) {
+        document.location.replace("/");
+    } else {
+        alert(response.statusText);
+    };
+};
+
+// event listener
+document.querySelector("#logout").addEventListener("click", logout);
