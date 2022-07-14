b5220b0ba9d6e859b8cce01c47a5269f2519008d
Ashley Smith <ashlynn4567@gmail.com>
2022-07-14 08:51:57 -0500

    proposed bug fix, not yet tested

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -6,11 +6,11 @@ const commentData = [];
 
 for (let i = 0; i < 20; i++) {
     // create a randomly generated paragraph 300 words or less
-    const comment_text = faker.lorem.words(Math.floor(Math.random() * 301));
+    const comment_text = faker.lorem.words(Math.floor(Math.random() * 300) + 1);
     // pick one of the 20 randomly generated posts
-    const post_id = Math.floor(Math.random() * 21);
+    const post_id = Math.floor(Math.random() * 20) + 1;
     // pick one of the 10 randomly generated users
-    const user_id = Math.floor(Math.random() * 11);
+    const user_id = Math.floor(Math.random() * 10) + 1;
 
     commentData.push({ comment_text, post_id, user_id });
 };
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -6,11 +6,11 @@ const postData = [];
 
 for (let i = 0; i < 20; i++) {
     // creates a random title 10 words or less
-    const title = faker.lorem.words(Math.floor(Math.random() * 11));
+    const title = faker.lorem.words(Math.floor(Math.random() * 10) + 1);
     // create a randomly generated paragraph 300 words or less
-    const body = faker.lorem.words(Math.floor(Math.random() * 301));
+    const body = faker.lorem.words(Math.floor(Math.random() * 300) + 1);
     // pick one of the 10 randomly generated users
-    const user_id = Math.floor(Math.random() * 11);
+    const user_id = Math.floor(Math.random() * 10) + 1;
 
     postData.push({ title, body, user_id });
 };
