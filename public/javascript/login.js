942798acd926790718d40da7cc2f6fb162344a70
Ashley's PC <ashlynn4567@gmail.com>
2022-07-14 20:49:12 -0500

    updated handlebars to have more consistent styling, updated stylesheet, connected front end and backend more seamlessly

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,13 +2,13 @@
 async function loginFormHandler(event) {
     event.preventDefault();
 
-    const email = document.querySelector("#email-login").value.trim();
-    const password = document.querySelector("#password-login").value.trim();
+    const username = document.getElementById("username-login").value.trim();
+    const password = document.getElementById("password-login").value.trim();
 
-    if (email && password) {
+    if (username && password) {
         const response = await fetch(`/api/users/login`, {
             method: "POST",
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify({ username, password }),
             headers: { "Content-Type": "application/json" }
         });
 
@@ -25,9 +25,9 @@ async function loginFormHandler(event) {
 async function signupFormHandler(event) {
     event.preventDefault();
 
-    const username = document.querySelector("#username-signup").value.trim();
-    const email = document.querySelector("#email-signup").value.trim();
-    const password = document.querySelector("#password-signup").value.trim();
+    const username = document.getElementById("username-signup").value.trim();
+    const email = document.getElementById("email-signup").value.trim();
+    const password = document.getElementById("password-signup").value.trim();
 
     if (username && email && password) {
         const response = await fetch(`/api/users`, {
